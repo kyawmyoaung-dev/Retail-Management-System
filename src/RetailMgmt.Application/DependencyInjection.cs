d2fdb997d4f1f16e4a0ad8f16f88230a1f27672d
Kyaw Myo Aung <info.kyawmyo@gmail.com>
2020-09-04 00:08:38 +0630

    Add Docfx Documentation Project

--- src/RetailMgmt.Application/DependencyInjection.cs
+++ src/RetailMgmt.Application/DependencyInjection.cs
@@ -6,8 +6,17 @@ using System.Reflection;
 
 namespace RetailMgmt.Application
 {
+    /// <summary>
+    /// Developer : Kyaw Myo Aung
+    /// DI class for all application services
+    /// </summary>
     public static class DependencyInjection
     {
+        /// <summary>
+        /// IServiceColection Extension Method
+        /// </summary>
+        /// <param name="services">IServiceColection Extension</param>
+        /// <returns></returns>
         public static IServiceCollection AddApplicationServices(this IServiceCollection services)
         {
             services.AddScoped<ICurrencyService, CurrencyService>();
